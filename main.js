393d88efb45620ad2030287e081861abaeec00a4
Victor Duarte <vicsalf@gmail.com>
2022-04-04 15:28:18 -0500

    Display on sale message

--- main.js
+++ main.js
@@ -1,24 +1,48 @@
 const app = Vue.createApp({
-    data() {
-        return {
-            cart:0,
-            product: 'Socks',
-            brand: 'Vue Mastery',
-            image: './assets/images/socks_blue.jpg',
-            inStock: false,
-            details: ['50% cotton', '30% wool', '20% polyester'],
-            variants: [
-              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
-            ]
-        }
-    },
-    methods: {
-        addToCart() {
-            this.cart += 1
+  data() {
+    return {
+      brand: "Vue Mastery",
+      cart: 0,
+      details: ["50% cotton", "30% wool", "20% polyester"],
+      onSale: true,
+      product: "Socks",
+      selectedVariant: 0,
+      variants: [
+        {
+          id: 2234,
+          color: "green",
+          image: "./assets/images/socks_green.jpg",
+          quantity: 50,
         },
-        updateImage(variantImage) {
-            this.image = variantImage
-        }
-    }
-})
+        {
+          id: 2235,
+          color: "blue",
+          image: "./assets/images/socks_blue.jpg",
+          quantity: 0,
+        },
+      ],
+    };
+  },
+  methods: {
+    addToCart() {
+      this.cart += 1;
+    },
+    updateVariant(index) {
+      this.selectedVariant = index;
+    },
+  },
+  computed: {
+    image() {
+      return this.variants[this.selectedVariant].image;
+    },
+    inStock() {
+      return this.variants[this.selectedVariant].quantity;
+    },
+    salesMessage() {
+      return this.brand + " " + this.product + " is on sale.";
+    },
+    title() {
+      return this.brand + " " + this.product;
+    },
+  },
+});
