fb37b453e2b3e394c0e761df8fd1ad1741a5e6e8
Victor Duarte <vicsalf@gmail.com>
2022-04-04 09:04:20 -0500

    Add a product description to the data object

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const app = Vue.createApp({
   data: function () {
     return {
+      description:
+        "A covering for the foot, typically made of leather, having a sturdy sole and not reaching above the ankle.",
       product: "Socks",
     };
   },
