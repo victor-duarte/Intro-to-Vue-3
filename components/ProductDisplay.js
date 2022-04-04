d3c319fd57999b55d56aac59a2f75c4f0477eb2b
Victor Duarte <vicsalf@gmail.com>
2022-04-04 18:24:35 -0500

    Add functionality to add and remove items to cart

--- components/ProductDisplay.js
+++ components/ProductDisplay.js
@@ -1,16 +1,20 @@
-app.component('product-display', {
+app.component("product-display", {
   props: {
+    cartLength: {
+      required: true,
+      type: Number,
+    },
     premium: {
+      required: true,
       type: Boolean,
-      required: true
-    }
+    },
   },
-  template: 
-  /*html*/
-  `<div class="product-display">
+  template:
+    /* html */
+    `<div class="product-display">
     <div class="product-container">
       <div class="product-image">
-        <img v-bind:src="image">
+        <img v-bind:src="image" alt="">
       </div>
       <div class="product-info">
         <h1>{{ title }}</h1>
@@ -24,59 +28,81 @@ app.component('product-display', {
           <li v-for="detail in details">{{ detail }}</li>
         </ul>
 
-        <div 
-          v-for="(variant, index) in variants" 
-          :key="variant.id" 
-          @mouseover="updateVariant(index)" 
-          class="color-circle" 
-          :style="{ backgroundColor: variant.color }">
-        </div>
+        <div
+          :key="variant.id"
+          :style="{ backgroundColor: variant.color }"
+          @mouseover="updateVariant(index)"
+          class="color-circle"
+          v-for="(variant, index) in variants"
+        ></div>
         
-        <button 
-          class="button" 
-          :class="{ disabledButton: !inStock }" 
-          :disabled="!inStock" 
-          v-on:click="addToCart">
+        <button
+          :class="{ disabledButton: !inStock }"
+          :disabled="!inStock"
+          class="button"
+          v-on:click="addToCart"
+        >
           Add to Cart
+        </button>
+        <button
+          :class="{ disabledButton: cartLength <= 0 }"
+          :disabled="cartLength <= 0"
+          @click="removeFromCart"
+          class="button"
+        >
+          Remove Item
         </button>
       </div>
     </div>
   </div>`,
   data() {
     return {
-        product: 'Socks',
-        brand: 'Vue Mastery',
-        selectedVariant: 0,
-        details: ['50% cotton', '30% wool', '20% polyester'],
-        variants: [
-          { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
-          { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
-        ]
-    }
+      brand: "Vue Mastery",
+      details: ["50% cotton", "30% wool", "20% polyester"],
+      product: "Socks",
+      selectedVariant: 0,
+      variants: [
+        {
+          color: "green",
+          id: 2234,
+          image: "./assets/images/socks_green.jpg",
+          quantity: 50,
+        },
+        {
+          color: "blue",
+          id: 2235,
+          image: "./assets/images/socks_blue.jpg",
+          quantity: 0,
+        },
+      ],
+    };
   },
   methods: {
-      addToCart() {
-          this.cart += 1
-      },
-      updateVariant(index) {
-          this.selectedVariant = index
-      }
+    addToCart() {
+      this.$emit("add-to-cart", this.variants[this.selectedVariant].id);
+    },
+    removeFromCart() {
+      this.$emit("remove-from-cart", this.variants[this.selectedVariant].id);
+    },
+    updateVariant(index) {
+      this.selectedVariant = index;
+    },
   },
   computed: {
-      title() {
-          return this.brand + ' ' + this.product
-      },
-      image() {
-          return this.variants[this.selectedVariant].image
-      },
-      inStock() {
-          return this.variants[this.selectedVariant].quantity
-      },
-      shipping() {
-        if (this.premium) {
-          return 'Free'
-        }
-        return 2.99
+    image() {
+      return this.variants[this.selectedVariant].image;
+    },
+    inStock() {
+      return this.variants[this.selectedVariant].quantity;
+    },
+    shipping() {
+      if (this.premium) {
+        return "Free";
       }
-  }
-})
+      return 2.99;
+    },
+    title() {
+      return this.brand + " " + this.product;
+    },
+  },
+});
--- main.js
+++ main.js
@@ -1,9 +1,19 @@
 const app = Vue.createApp({
-    data() {
-        return {
-            cart: 0,
-            premium: true
-        }
+  data() {
+    return {
+      cart: [],
+      premium: true,
+    };
+  },
+  methods: {
+    addToCart(id) {
+      this.cart.push(id);
     },
-    methods: {}
-})
+    removeFromCart(id) {
+      const index = this.cart.indexOf(id);
+      if (index > -1) {
+        this.cart.splice(index, 1);
+      }
+    },
+  },
+});
