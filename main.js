85f3adbe2faa2251104c65c5a3769d5ae3532fad
Victor Duarte <vicsalf@gmail.com>
2022-04-04 09:39:21 -0500

    Bind data properties to tags

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 const app = Vue.createApp({
-    data() {
-        return {
-            product: 'Socks'
-        }
-    }
-})
+  data() {
+    return {
+      alt: "Athletic Socks Outdoor Sport Socks Thigh High Tights Stockings Casual Stripes Tube Socks",
+      image: "./assets/images/socks_green.jpg",
+      product: "Socks",
+      url: "https://www.google.com/search?q=green+socks&bih=953&biw=1018&hl=en&sxsrf=APq-WBtCUgG96TMkYuThvTzyi1zQ20iDcQ%3A1649083078312&ei=xgJLYp7gEuibwbkPy6ODyA8&ved=0ahUKEwie29mY0fr2AhXoTTABHcvRAPkQ4dUDCA4&uact=5&oq=green+socks&gs_lcp=Cgdnd3Mtd2l6EAMyBwgjELADECcyBwgAEEcQsAMyBwgAEEcQsAMyBwgAEEcQsAMyBwgAEEcQsAMyBwgAEEcQsAMyBwgAEEcQsAMyBwgAEEcQsAMyBwgAEEcQsAMyBwgAELADEENKBAhBGABKBAhGGABQsBRYthVgyxVoAXABeACAAQCIAQCSAQCYAQCgAQHIAQrAAQE&sclient=gws-wiz",
+    };
+  },
+});
