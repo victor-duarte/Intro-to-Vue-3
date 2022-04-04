7e1b445446ec161f8c714da154d18726b9ad33f3
Victor Duarte <vicsalf@gmail.com>
2022-04-04 13:56:14 -0500

    Bind add and remove from cart events

--- main.js
+++ main.js
@@ -1,15 +1,30 @@
 const app = Vue.createApp({
-    data() {
-        return {
-            cart:0,
-            product: 'Socks',
-            image: './assets/images/socks_blue.jpg',
-            inStock: true,
-            details: ['50% cotton', '30% wool', '20% polyester'],
-            variants: [
-              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
-            ]
-        }
-    }
-})
+  data() {
+    return {
+      cart: 0,
+      details: ["50% cotton", "30% wool", "20% polyester"],
+      image: "./assets/images/socks_blue.jpg",
+      inStock: true,
+      product: "Socks",
+      variants: [
+        { id: 2234, color: "green", image: "./assets/images/socks_green.jpg" },
+        { id: 2235, color: "blue", image: "./assets/images/socks_blue.jpg" },
+      ],
+    };
+  },
+  methods: {
+    addToCart() {
+      this.cart += 1;
+    },
+    removeFromCart() {
+      if (this.cart <= 0) {
+        return;
+      }
+
+      this.cart -= 1;
+    },
+    updateImage(variatImage) {
+      this.image = variatImage;
+    },
+  },
+});
